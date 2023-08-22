95609a4e527a7350e6da66c3f108238660a77499
devFerC <dev.fer.c@gmail.com>
2023-08-21 23:35:35 -0400

    Production url updated

--- src/core/utils/index.js
+++ src/core/utils/index.js
@@ -1,3 +1,3 @@
 const production= process.env.NODE_ENV === "production";
 
-export const SITE_URL= production ? "prod-url" : "http://localhost:3000";
+export const SITE_URL= production ? "https://soundscribed.online" : "http://localhost:3000";
